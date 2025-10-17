3347b6bc2f28d9551a7a13da968a13752955d36a
strngpowty <strngpowty@gmail.com>
2025-10-17 07:09:46 +0530

    commit issue solved

---
+++ src/hello.js
@@ -0,0 +1,6 @@
+var username = "Aishwarya";
+var age = 24;
+function greetUser(username, age) {
+    return "Hello " + username + "you are " + age + " years old";
+}
+console.log(greetUser(username, age));
---
+++ src/hello.ts
@@ -0,0 +1,8 @@
+let username: string = "Aishwarya"
+let age: number = 24
+
+function greetUser(username: string, age: number) {
+    return "Hello " + username + "you are " + age + " years old"
+}
+
+console.log(greetUser(username, age))
